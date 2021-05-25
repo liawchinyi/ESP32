7e42999b009460614ff34e76fbd14fcad621d829
Liaw <liawchinyi@hotmail.com>
2021-05-25 13:00:16 +0800

    main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # UPYBBOT - micropython balaancing robot
+# Complete project details at https://RandomNerdTutorials.com
+
+import wifimgr
+from time import sleep
 
 import esp32
 import machine 
@@ -6,19 +10,59 @@ import time
 from time import ticks_us,ticks_cpu
 from machine import Pin
 from machine import Timer
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
+
+# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
+print("ESP OK")
+
+def web_page():
+  if led.value() == 1:
+    gpio_state="ON"
+  else:
+    gpio_state="OFF"
+  
+  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
+  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
+  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
+  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+  return html
+  
+try:
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  s.bind(('', 80))
+  s.listen(5)
+except OSError as e:
+  machine.reset()
+
 # set up stepper motors
 
 from nemastepper import Stepper
 from nemastepper import Stepper
 motor1 = Stepper(26,25,12)
 motor2 = Stepper(33,32,14)
-
+led=Pin(19,Pin.OUT)        #create LED object from pin2,Set Pin2 to output
 def step_cb(t):
     motor1.do_step()
     motor2.do_step()
     
 timer=Timer(8)
-timer.init(freq=10000, mode=Timer.PERIODIC, callback=step_cb)   #initializing the timer
+timer.init(freq=20000, mode=Timer.PERIODIC, callback=step_cb)   #initializing the timer
 
 motor1.MAX_ACCEL = 1000  
 motor2.MAX_ACCEL = 1000  
@@ -26,12 +70,46 @@ speed = 3000
 motor1.set_speed(speed)
 motor2.set_speed(speed)
 print ('press button to stop')
+
 while 1 :
-    print (speed)
-print ('button pressed')
+  led.value(1)            #Set led turn on
+  time.sleep(0.1)
+  led.value(0)            #Set led turn off
+  time.sleep(0.1)
+  
+  try:
+    if gc.mem_free() < 102000:
+      gc.collect()
+    conn, addr = s.accept()
+    conn.settimeout(3.0)
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    conn.settimeout(None)
+    request = str(request)
+    print('Content = %s' % request)
+    led_on = request.find('/?led=on')
+    led_off = request.find('/?led=off')
+    if led_on == 6:
+      print('LED ON')
+      led.value(1)
+    if led_off == 6:
+      print('LED OFF')
+      led.value(0)
+    response = web_page()
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()
+  except OSError as e:
+    conn.close()
+    print('Connection closed')
+    
+print ('end')
     
 motor1.set_speed(0)
 motor1.set_off()
 
 timer.deinit()
+
 
