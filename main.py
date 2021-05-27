e8ecb64630a9c65aecd1f21111b36eac2fdb50ae
Liaw <liawchinyi@hotmail.com>
2021-05-27 20:27:13 +0800

    gyro

--- main.py
+++ main.py
@@ -9,18 +9,14 @@ import wifimgr
 import time
 from time import ticks_us,ticks_cpu, sleep
 import machine
-import machine 
-from machine import Pin, Timer
+from machine import I2C, Pin, Timer, sleep
 
-try:
-  import usocket as socket
-except:
-  import socket
+import mpu6050
+i2c = I2C(scl=Pin(22), sda=Pin(21))     #initializing the I2C method for ESP32
+mpu= mpu6050.accel(i2c)
 
 # set up stepper motors
-
 from nemastepper import Stepper
-
 motor1 = Stepper(26,25,12)
 motor2 = Stepper(33,32,14)
 led = Pin(19, Pin.OUT)
@@ -44,76 +40,15 @@ speed = 3000
 motor1.set_speed(speed)
 motor2.set_speed(speed)
 
-wlan = wifimgr.get_connection()
-if wlan is None:
-    print("Could not initialize the network connection.")
-    while True:
-        pass  # you shall not pass :D
-
-# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
-print("ESP OK")
-
-def web_page():
-  if led.value() == 1:
-    gpio_state="ON"
-  else:
-    gpio_state="OFF"
-  
-  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
-  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
-  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
-  return html
-  
-try:
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  s.bind(('', 80))
-  s.listen(5)
-except OSError as e:
-  machine.reset()
-
-def update_socket(self):
-    while True:
-        try:
-            if gc.mem_free() < 102000:
-                gc.collect()
-            conn, addr = s.accept()
-            conn.settimeout(3.0)
-            print('Got a connection from %s' % str(addr))
-            request = conn.recv(1024)
-            conn.settimeout(None)
-            request = str(request)
-            print('Content = %s' % request)
-            led_on = request.find('/?led=on')
-            led_off = request.find('/?led=off')
-            if led_on == 6:
-                print('LED ON')
-                led.value(1)
-            if led_off == 6:
-                print('LED OFF')
-                led.value(0)
-            response = web_page()
-            conn.send('HTTP/1.1 200 OK\n')
-            conn.send('Content-Type: text/html\n')
-            conn.send('Connection: close\n\n')
-            conn.sendall(response)
-            conn.close()
-        except OSError as e:
-            conn.close()
-            print('Connection closed')
-
 #timer3=Timer(7)
 #timer3.init(freq=10, mode=Timer.PERIODIC, callback=update_socket)   #initializing the timer
 
 print ('start')
 
 while True:
-    pass
-
+    mpu.get_values()
+    print(mpu.get_values())
+    sleep(500)
 
 print ('exit')
     
