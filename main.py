5f37a8752683d5ee683501b44e4a156533e48b46
Liaw <liawchinyi@hotmail.com>
2021-05-30 13:56:24 +0800

    quit

--- main.py
+++ main.py
@@ -4,67 +4,148 @@
 # C:\Python39\Lib\site-packages>esptool.py erase_flash
 # C:\Python39\Lib\site-packages>esptool.py --port COM3 write_flash 0x1000 C:\ESP32a\esp32-20210418-v1.15.bin
 # Complete project details at https://RandomNerdTutorials.com
-
+# jeffm   - 23.4.2015
 import time
-from time import ticks_us,ticks_cpu, sleep
+import sys
+from time import ticks_us, ticks_cpu, sleep
 import machine
 from machine import I2C, Pin, Timer, sleep
 
-import mpu6050
-i2c = I2C(scl = Pin(22), sda = Pin(21), freq = 400000)
-#initializing the I2C method for ESP32
-mpu= mpu6050.accel(i2c)
-
 # set up stepper motors
-from stepper import Stepper
-#motor1 = Stepper(26,25,12)
-#motor2 = Stepper(33,32,14)
-motor1 = Stepper(25,26,12)
-motor2 = Stepper(32,33,14)
+from nemastepper import Stepper
+motor1 = Stepper(26,25,12)#nemastepper
+motor2 = Stepper(33,32,14)#nemastepper
+#motor1 = Stepper(25,26,12)
+#motor2 = Stepper(32,33,14)
 
 led = Pin(19, Pin.OUT)
-limit_sw = Pin(0, Pin.IN, Pin.PULL_UP)#输入红外探头
+led.value(1)
+BOOT_sw = Pin(0, Pin.IN, Pin.PULL_UP)#输入红外探头
+
+if BOOT_sw.value() == 0:
+    sys.exit()
+    
+import mpu6050
+i2c = I2C(scl = Pin(22), sda = Pin(21), freq = 400000)
+
+#initializing the I2C method for ESP32
+imu= mpu6050.accel(i2c)
+
 def step_cb(self):
     global motor1, motor2
-    #motor1.do_step()
-    #motor2.do_step()
-    motor1.steps(10)
-    motor2.steps(10)
+    motor1.do_step()
+    motor2.do_step()
+    #motor1.steps(10)
+    #motor2.steps(10)
 
 def led_cb(self):
+    global led
     led.value(not led.value())
 
 #initializing the timer
 timer=Timer(4)
-timer.init(freq=1, mode=Timer.PERIODIC, callback=led_cb)#initializing the timer
+timer.init(freq=2, mode=Timer.PERIODIC, callback=led_cb)
+#initializing the timer
 
-timer2=Timer(6)
-timer2.init(freq=20000, mode=Timer.PERIODIC, callback=step_cb)   #initializing the timer
+tim=Timer(6)
+#tim.init(freq=20000, mode=Timer.PERIODIC, callback=step_cb)   
+#initializing the timer
 
 motor1.MAX_ACCEL = 1000  
 motor2.MAX_ACCEL = 1000  
 speed = 3000
-#motor1.set_speed(speed)
-#motor2.set_speed(speed)
-motor1.set_step_time(60)
-motor2.set_step_time(60)
-array = mpu.get_values()
+motor1.set_speed(speed)
+motor2.set_speed(speed)
+#motor1.set_step_time(1)
+#motor2.set_step_time(1)
+
+# Complementary Filter A = rt/(rt + dt) where rt is response time, dt = period
+def compf(fangle,accel,gyro,looptime,A):
+    fangle = A * (fangle + gyro * looptime/1000000) + (1-A) * accel
+    return fangle
+
+MAX_VEL = 2000 # 2000 usteps/sec = 500steps/sec = 2.5rps = 150rpm
+MAX_ANGLE = 10  # degrees of tilt for speed control
+
+def constrain(val,minv,maxv):
+    if val<minv:
+        return minv
+    elif val>maxv:
+        return maxv
+    else:
+        return val
+
+#stability PD controiller - input is target angle, output is acceleration
+K = 6 # 7
+Kp = 4.0
+Kd = 0.5
+
+def stability(target,current,rate):
+    global K,Kp,Kd
+    error = target - current
+    output = Kp * error - Kd*rate
+    return int(K*output)
+
+#speed P controiller - input is target speed, output is inclination angle
+KpS = 0.01
+def speedcontrol(target,current):
+    global KpS
+    error = target - current
+    output = KpS * error 
+    return constrain(output,-MAX_ANGLE,+MAX_ANGLE)
+
+#main balance loop runs every 5ms
+def balance():
+    gangle = 0.0
+    start = ticks_us()
+    controlspeed = 0
+    fspeed = 0
+    while abs(gangle) < 45:  # give up if inclination angle >=45 degrees
+        angle  = imu.pitch()
+        rate   = imu.get_gy()
+        gangle = compf(gangle, angle, rate, (ticks_us()-start),0.99)         
+        start = ticks_us()
+        # speed control
+        actualspeed = (motor1.get_speed()+motor2.get_speed())/2
+        fspeed = 0.95 * fspeed + 0.05 * actualspeed
+        cmd = 0 #radio.poll() # cmd[0] is turn speed, cmd[1] is fwd/rev speed
+        tangle = speedcontrol(800*cmd[1],fspeed)
+         # stability control
+        controlspeed += stability(tangle, gangle, rate)           
+        controlspeed = constrain(controlspeed,-MAX_VEL,MAX_VEL)
+        # set motor speed
+        motor1.set_speed(-controlspeed-int(300*cmd[0]))
+        motor2.set_speed(-controlspeed+int(300*cmd[0]))
+        sleep(5000-(ticks_us()-start))
+    # stop and turn off motors
+    motor1.set_speed(0)
+    motor2.set_speed(0)
+    motor1.set_off()
+    motor2.set_off()
+
+array = imu.get_values()
 print(array)
 print ('start')
 count = 0
-while count < 50000 and limit_sw.value() == 1:
+while count < 5000 and limit_sw.value() == 1:
     count = count + 1
-    array = mpu.get_values()
+    array = imu.get_values()
     speed = array["GyY"]+array["AcX"]
-
+    tim.init(freq=20000, mode=Timer.PERIODIC, callback=step_cb) #start interrupt routine
+    balance()
+    tim.init(callback=None)
     #print(count, speed, array)
     #sleep(100)
 
 print ('exit')
-print(array)    
+print(array)   
+
 motor1.set_speed(0)
 motor1.set_off()
 motor2.set_speed(0)
 motor2.set_off()
+
+#motor1.power_off()
+#motor2.power_off()
 timer.deinit()
-timer2.deinit()
+tim.deinit()
--- mpu6050.py
+++ mpu6050.py
@@ -1,12 +1,140 @@
 import machine
-
+import os
+from struct import unpack as unp
+from math import atan2,degrees,pi
+'''
+def ar(i):
+    switcher={
+            0: 0x00,
+            1: 0x08,
+            2: 0x10,
+            3: 0x18
+            }
+    return switcher.get(i,"Invalid")
+'''
 class accel():
-    def __init__(self, i2c, addr=0x68):
+    '''
+    Module for the MPY6050 6DOF IMU. 
+    By default interrupts are disabled while reading or writing to the device. This
+    prevents occasional bus lockups in the presence of pin interrupts, at the cost
+    of disabling interrupts for about 250uS.
+    '''
+    mpu_addr = 0x68  # address of MPU6050
+    _I2Cerror = "I2C communication failure"
+
+    def __init__(self, i2c, addr=0x68, disable_interrupts=False):
         self.iic = i2c
+        self.iic.scan()
         self.addr = addr
         self.iic.start()
         self.iic.writeto(self.addr, bytearray([107, 0]))
         self.iic.stop()
+
+        # now apply user setting for interrupts
+        self.disable_interrupts = disable_interrupts        
+        # wake it up
+        self.wake()
+        self.accel_range(1)
+        self._ar = self.accel_range()
+        self.gyro_range(0)
+        self._gr = self.gyro_range()
+
+    # wake
+    def wake(self):
+        '''
+        Wakes the device.
+        '''
+        try:
+            self._write(bytearray(0x01), 0x6B, self.mpu_addr)
+        except OSError:
+            print(accel._I2Cerror)
+        return 'awake'
+
+    # read from device
+    def _read(self, count, memaddr, devaddr):
+        '''
+        Perform a memory read. Caller should trap OSError. Possible values of
+        error args[0]: errorno.ETIMEDOUT errno.EBUSY or errno.EIO
+        '''
+        irq_state = True
+        if self.disable_interrupts:
+            irq_state = machine.disable_irq()
+        self.iic.start()
+        result = self.iic.readfrom_mem(
+                                        devaddr,
+                                        memaddr, count)
+        self.iic.stop()                                
+        machine.enable_irq(irq_state)
+        return result
+
+    # write to device
+    def _write(self, data, memaddr, devaddr):
+        '''
+        Perform a memory write. Caller should trap OSError.
+        '''
+        irq_state = True
+        if self.disable_interrupts:
+            irq_state = machine.disable_irq()
+        self.iic.start()    
+        result = self.iic.writeto_mem(
+                                         devaddr,
+                                         memaddr, data)
+        self.iic.stop()                                   
+        machine.enable_irq(irq_state)
+        return result
+
+    # accelerometer range
+    def accel_range(self, accel_range=None):
+        '''
+        Returns the accelerometer range or sets it to the passed arg.
+        Pass:               0   1   2   3
+        for range +/-:      2   4   8   16  g 
+        '''
+        # set range
+        try:
+            if accel_range is None:
+                pass
+            else:
+                arl = [0x00, 0x08, 0x10, 0x18]                
+                ar = arl[accel_range]
+                try:
+                    self._write(bytearray(ar), 0x1C, self.addr)
+                except IndexError:
+                    print('accel_range can only be 0, 1, 2 or 3')
+            # get range
+            ari = int(unp('<H', self._read(1, 0x1C, self.addr))[0]/8)
+        except OSError:
+            ari = None
+        if ari is not None:
+            self._ar = ari
+        return ari
+
+    # gyroscope range
+    def gyro_range(self, gyro_range=None):
+        '''
+        Returns the gyroscope range or sets it to the passed arg.
+        Pass:               0   1   2    3
+        for range +/-:      250 500 1000 2000  degrees/second
+        '''
+        # set range
+        try:
+            if gyro_range is None:
+                pass
+            else:
+                grl = [0x00, 0x08, 0x10, 0x18]
+                gr = grl[gyro_range]
+                try:
+                    self._write(bytearray(gr), 0x1B, self.addr)              
+                except IndexError:
+                    print('gyro_range can only be 0, 1, 2 or 3')
+            # get range
+            gri = int(unp('<H', self._read(1, 0x1B, self.addr))[0]/8)
+        except OSError:
+            gri = None
+
+        if gri is not None:
+            self._gr = gri
+        return gri
 
     def get_raw_values(self):
         self.iic.start()
@@ -44,3 +172,47 @@ class accel():
         while 1:
             print(self.get_values())
             sleep(0.05)
+
+    # get raw acceleration
+    def get_accel_raw(self):
+        '''
+        Returns the accelerations on xyz in bytes.
+        '''
+        try:
+            axyz = self._read(6, 0x3B, self.mpu_addr)
+        except OSError:
+            axyz = b'\x00\x00\x00\x00\x00\x00'
+        return axyz
+
+    # get pitch  
+    def pitch(self):
+        '''
+        Returns pitch angle in degrees based on x and c accelerations.
+        
+        '''
+        scale = (16384, 8192, 4096, 2048)
+        raw = self.get_accel_raw()
+        x = unp('>h', raw[0:2])[0]/scale[self._ar]
+        z = unp('>h', raw[4:6])[0]/scale[self._ar]
+        pitch = degrees(pi+atan2(-x,-z))
+        if (pitch>=180) and (pitch<=360):
+            pitch-=360
+        return -pitch
+
+    # get raw gyro
+    def get_gyro_raw(self):
+        '''
+        Returns the turn rate on xyz in bytes.
+        '''
+        try:
+            gxyz = self._read(6, 0x43, self.mpu_addr)
+        except OSError:
+            gxyz = b'\x00\x00\x00\x00\x00\x00'
+        return gxyz        
+
+    # get gyro pitch - y - axis in degrees
+    def get_gy(self):
+        scale = (131.0, 65.5, 32.8, 16.4)
+        raw = self.get_gyro_raw()
+        gy =  unp('>h', raw[2:4])[0]/scale[self._gr]
+        return gy    
